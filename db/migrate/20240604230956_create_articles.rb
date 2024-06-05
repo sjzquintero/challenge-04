0521e5341d4c62cdad97d59528b80d2bb9123e47
Santiago Jimenez <squintero@rrc.ca>
2024-06-05 11:14:11 -0500

    first commit of the U4

---
+++ db/migrate/20240604230956_create_articles.rb
@@ -0,0 +1,11 @@
+bin/rails db:migrate
+class CreateArticles < ActiveRecord::Migration[7.1]
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.text :body
+
+      t.timestamps
+    end
+  end
+end
